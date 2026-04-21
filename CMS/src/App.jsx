cacca3a710101e4015b0caefbe9997eee68b1c48
Thebe Khabo <thebe.khabo1@gmail.com>
2026-04-21 19:36:32 +0200

    Pages folder created and Navigation set

--- CMS/src/App.jsx
+++ CMS/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import { Layout } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
-    <div>Welcome to the CMS</div>
-  );
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
